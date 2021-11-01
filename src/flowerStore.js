ded1bfba1874f5101b3ea48b7e6de60ae743c850
DmitryDenisuyk <dmitrijddm@gmail.com>
2021-11-01 22:03:08 +0300

    com2

--- src/flowerStore.js
+++ src/flowerStore.js
@@ -8,23 +8,23 @@ class FlowerStore {
 
     constructor() {
         this.GardenFlowers =  [
-            new GardenFlower('red', 60, 6, 10),
-            new GardenFlower('white', 60, 5, 10),
-            new GardenFlower('yellow', 50, 3, 10),
+            new GardenFlower('red', 60, 6, 15),
+            new GardenFlower('white', 60, 5, 12),
+            new GardenFlower('yellow', 50, 3, 11),
             new GardenFlower('blue', 50, 2, 10),
-            new GardenFlower('pink', 70, 10, 10),
+            new GardenFlower('pink', 70, 10, 17),
         ]; 
         this.FieldFlowers = [
-            new FieldFlower('red', 60, 'rare', 10),
+            new FieldFlower('red', 60, 'rare', 20),
             new FieldFlower('green', 70, 'uncommon', 10),
-            new FieldFlower('yellow', 60, 'common', 10),
-            new FieldFlower('white', 50, 'uncommon', 10),
+            new FieldFlower('yellow', 60, 'common', 15),
+            new FieldFlower('white', 50, 'uncommon', 19),
         ];
         this.HomeFlowers = [
-            new HomeFlower('brown', 50, false, 10),
-            new HomeFlower('red', 70, true, 10),
-            new HomeFlower('blue', 50, true, 10),
-            new HomeFlower('orange', 60, true, 10),
+            new HomeFlower('brown', 50, false, 12),
+            new HomeFlower('red', 70, true, 11),
+            new HomeFlower('blue', 50, true, 7),
+            new HomeFlower('orange', 60, true, 9),
         ];
         this.Packs = [
             new Pack('paper', 5),
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const prompt = require('../node_modules/prompt-promise');
-// const readline = require('../node_modules/readline-sync');
 let FieldFlower = require('../src/buisnes/flowers/fieldFlowers/fieldFlower');
 let GardenFlower = require('../src/buisnes/flowers/gardenFlowers/gardenFlower');
 let HomeFlower = require('../src/buisnes/flowers/homeFlowers/homeFlower');
@@ -13,74 +12,36 @@ let store = new FlowerStore();
 
 (async () => {
     let kindFlowers = 0;
-    while(kindFlowers != 4){
+    while (kindFlowers != 4) {
 
-    kindFlowers = parseInt(await prompt('What kind of flowers do you need? (field - 1, garden - 2, home - 3 flowers) 4 - exit '));
-    if (kindFlowers === 1) {
-        console.log(store.getFieldFlowers());
-        const whichFlower = parseInt(await prompt('Which flower do you want to choose? '));
-        bouquet.addFieldFlower(store.getFieldFlowers()[whichFlower]);
-        console.log('Your choice is: ', (store.getFieldFlowers()[whichFlower]));
-    } else if (kindFlowers === 2) {
-        console.log(store.getGardenFlowers());
-        const whichFlower = parseInt(await prompt('Which flower do you want to choose? '));
-        bouquet.addGardenFlower(store.getGardenFlowers()[whichFlower]);
-        console.log('Your choice is: ', (store.getGardenFlowers()[whichFlower]));
-    } else if (kindFlowers === 3) {
-        console.log(store.getHomeFlowers());
-        const whichFlower = parseInt(await prompt('Which flower do you want to choose? '));
-        bouquet.addHomeFlower(store.getHomeFlowers()[whichFlower]);
-        console.log('Your choice is: ', (store.getHomeFlowers()[whichFlower]));
+        kindFlowers = parseInt(await prompt('What kind of flowers do you need? (field - 1, garden - 2, home - 3 flowers) 4 - exit '));
+        if (kindFlowers === 1) {
+            console.log(store.getFieldFlowers());
+            const whichFlower = parseInt(await prompt('Which flower do you want to choose? '));
+            bouquet.addFieldFlower(store.getFieldFlowers()[whichFlower]);
+            console.log('Your choice is: ', (store.getFieldFlowers()[whichFlower]));
+        } else if (kindFlowers === 2) {
+            console.log(store.getGardenFlowers());
+            const whichFlower = parseInt(await prompt('Which flower do you want to choose? '));
+            bouquet.addGardenFlower(store.getGardenFlowers()[whichFlower]);
+            console.log('Your choice is: ', (store.getGardenFlowers()[whichFlower]));
+        } else if (kindFlowers === 3) {
+            console.log(store.getHomeFlowers());
+            const whichFlower = parseInt(await prompt('Which flower do you want to choose? '));
+            bouquet.addHomeFlower(store.getHomeFlowers()[whichFlower]);
+            console.log('Your choice is: ', (store.getHomeFlowers()[whichFlower]));
+        }
     }
-}
 
-let packNeeded = parseInt(await prompt('Do you wan to use the pack for your bouqet? '));
-if (packNeeded === 1) {
-    console.log(store.Packs);
-    const whichPack = parseInt(await prompt('Which pack do you want to choose? '));
-    bouquet.addPack(store.Packs[whichPack]);
+    let packNeeded = parseInt(await prompt('Do you want to use the pack for your bouqet? (1 - yes) '));
+    if (packNeeded === 1) {
+        console.log(store.Packs);
+        const whichPack = parseInt(await prompt('Which pack do you want to choose? '));
+        bouquet.addPack(store.Packs[whichPack]);
+    }
 
-}
-
-
- 
-
-    //const numberFlower = parseInt(await prompt('How many flowers do you want to buy? '));
 
     console.log('You have to pay', bouquet.getTotalCost());
-
-
-
-
-    // const color = await prompt('enter color: ');
-    // // let idxColor = await fieldFlower.indexOf(color);
-    // const length = parseInt(await prompt('enter length: '));
-    // // let idxLength = await fieldFlower.indexOf(length);
-    // // let rarity, time, sunNeeded;
-
-    // if (kindFlowers === 1) {
-    //     rarity = await prompt('Enter rarity: ');
-
-    //     // let arrFieldFlower = [color, length, rarity]; 
-    //     console.log(arrFieldFlower);
-    // } 
-    // else if (kindFlowers === 2) {
-    //     time = await prompt('Enter TTL of flower: ');
-
-    //     // let arrGardenFlower = [color, length, time]; 
-    // } 
-    // else if (kindFlowers === 3) {
-    //     // sunNeeded = await prompt('enter need sun: ');
-
-    //     // let arrHomeFlower = [color, length, sunNeeded]; 
-    // } 
-
-
-
-    // while (idx != -1) {
-    //   indices.push(idx);
-    //   idx = array.indexOf(element, idx + 1);
-    // }
 
 
     prompt.finish();
